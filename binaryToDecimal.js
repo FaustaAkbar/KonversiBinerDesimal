5d1988d68424852d3d5e7fd7dc20dc4f6652ec66
FaustaAkbar <faussss1357@gmail.com>
2024-05-30 22:41:11 +0700

    menambahkan function bruteforce

---
+++ binaryToDecimal.js
@@ -0,0 +1,20 @@
+//algoritma bruteforce
+function bruteforce(binary) {
+    for (const bit of binary) {
+        if (bit !== '0' && bit !== '1') {
+            return null;
+        }
+    }
+    
+    let desimal = 0;
+    let pangkat = 0;
+
+    for (let i = binary.length - 1; i >= 0; i--) {
+        if (binary[i] === '1') {
+            desimal += Math.pow(2, pangkat);
+        }
+        pangkat++;
+    }
+
+    return desimal;
+}
